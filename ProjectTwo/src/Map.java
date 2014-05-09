0cd421069d8e511233f1c5792d13fc8cfe5a5951
BaselNash <basil_nash@hotmail.com>
2014-05-09 18:51:21 -0400

    cc
    cc

--- ProjectTwo/src/Map.java
+++ ProjectTwo/src/Map.java
@@ -1,41 +1,167 @@
 public class Map {
 
 	public Locale currentLocale;
-	public Locale newLocale;
 	public Locale[] Locations;
 	public Item[] localeItems;
 
-	public void moveNorth() {
+	pbulic getQueue(){
+		
+		public Queue() {
+			init();
+		}
+
+		//
+		// Public
+		//
+		public void enqueue(Locale Locations) throws Exception {
+			// Check for overflow.
+			if (backPtr < CAPACITY - 1) {
+				backPtr = backPtr + 1;
+				arr[backPtr] = Locations;
+			} else {
+				throw new Exception();
+			}
+		}
+
+		public Locale dequeue() throws Exception {
+			Locale retVal = null;
+			// Check for underflow.
+
+			if (!this.isEmpty()) {
+				retVal = arr[frontPtr];
+				// Shift every element towards the front.
+				for (int i = 0; i < backPtr; i++) {
+					arr[i] = arr[i + 1];
+				}
+				// Reinitialize the last element
+				arr[backPtr] = null;
+				// shift the back pointer towards the front.
+				backPtr--;
+			} else {
+				// In case of underflow, throw an underflow exception.
+				throw new Exception();
+
+			}
+			return retVal;
+		}
+
+		public boolean isEmpty() {
+			boolean retVal = false;
+			if (backPtr == -1) {
+				retVal = true;
+			}
+			return retVal;
+		}
+
+		public int getCapacity() {
+			return CAPACITY;
+		}
+
+		//
+		// Private
+		//
+		private void init() {
+			for (int i = 0; i < CAPACITY; i++) {
+				arr[i] = null;
+			}
+		}
+
+		private final int CAPACITY = 70;
+		private Locale[] arr = new Locale[CAPACITY];
+		private int frontPtr = 0;
+		private int backPtr = -1;
+	}
+
+	public getStack(){
+		
+		public Stack() {
+		init();
+	}
+
+	public void push(Locale Locations) throws Exception {
+		// Check for stack overflow.
+		if (topPtr > 0) {
+			topPtr = topPtr - 1;
+			arr[topPtr] = Locations;
+		} else {
+			// TODO: Throw an overflow exception.
+			throw new Exception();
+		}
+	}
+
+	public Locale pop() throws Exception {
+		Locale retVal = null;
+		// Check for stack underflow.
+		if (topPtr < CAPACITY) {
+			retVal = arr[topPtr];
+			topPtr = topPtr + 1;
+		} else {
+			// In case of underflow, return -1.
+			// TODO: Throw an underflow exception.
+			throw new Exception();
+
+		}
+		return retVal;
+	}
+
+	public boolean isEmpty() throws Exception {
+		boolean retVal = false;
+		if (topPtr == CAPACITY) {
+			retVal = true;
+		}
+		return retVal;
+	}
+
+	public boolean isEmptyMo() {
+		return (topPtr == CAPACITY);
+	}
+
+	//
+	// Private
+	//
+	private final int CAPACITY = 70;
+	private Locale[] arr = new Locale[CAPACITY];
+	private int topPtr = 0;
+
+	private void init() {
+		for (int i = 0; i < CAPACITY; i++) {
+			arr[i] = null;
+		}
+		topPtr = CAPACITY;
+	}
+	}
+
+	public boolean moveNorth() {
 		Locale newLocale = currentLocale.getNorth();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public void moveSouth() {
+	public boolean moveSouth() {
 		Locale newLocale = currentLocale.getSouth();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public void moveWest() {
+	public boolean moveWest() {
 		Locale newLocale = currentLocale.getWest();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public void moveEast() {
+	public boolean moveEast() {
 		Locale newLocale = currentLocale.getEast();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public void moveUp() {
+	public boolean moveUp() {
 		Locale newLocale = currentLocale.getUp();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public void moveDown() {
+	public boolean moveDown() {
 		Locale newLocale = currentLocale.getDown();
-		locationCheck();
+		return locationCheck(newLocale);
 	}
 
-	public Boolean locationCheck() {
+	public Boolean locationCheck(Locale newLocale) {
 
 		if (newLocale == null) {
 			return false;
